080c24e0e6f9dc40e2c7e61688820825375fee3c
Ritwick Malhotra <malhotraritwick2011@gmail.com>
2024-11-04 17:58:01 +0100

    updated icons on aside menu

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,7 +1,16 @@
+sidekiq_config = { 
+  url: ENV['REDIS_URL'],
+  ssl_params: { verify_mode: OpenSSL::SSL::VERIFY_NONE }
+}
+
 Sidekiq.configure_server do |config|
   config.logger.level = Logger::DEBUG
+  config.redis = sidekiq_config
 end
 
 Sidekiq.configure_client do |config|
   config.logger.level = Logger::DEBUG
+  config.redis = sidekiq_config
 end
+
+Sidekiq.default_job_options = { 'backtrace' => true }
